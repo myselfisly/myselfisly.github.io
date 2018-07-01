80c8988c9ed285c5995a2f6a5915e32635a474da
myselfisly <myselfisjue@163.com>
2018-07-01 15:40:49 +0800

    Site updated: 2018-07-01 15:40:47

--- js/totop.js
+++ js/totop.js
@@ -1,20 +1,27 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
+(function($) { 
+	// When to show the scroll link
+	// higher number = scroll link appears further down the page：显示返回顶部的位置
+	var upperLimit = 1000;
+	
+	// Our scroll link element
+	var scrollElem = $('#totop');
+   
+	// Scroll to top speed：回滚速度
+	var scrollSpeed = 500;
+   
+	// Show and hide the scroll to top link based on scroll position   
+	scrollElem.hide();
+	$(window).scroll(function () {            
+		var scrollTop = $(document).scrollTop();       
+		if ( scrollTop > upperLimit ) {
+			$(scrollElem).stop().fadeTo(300, 1); // fade back in           
+		}else{       
+			$(scrollElem).stop().fadeTo(300, 0); // fade out
+		}
+	});
 
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 1000, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
-
-$("#homelogo").click(function() {
-    $("html, body").animate({
-        scrollTop: $(window).height()
-    }, 1000, null);
-    return false;
-});
+	// Scroll to top animation on click
+	$(scrollElem).click(function(){
+		$('html, body').animate({scrollTop:0}, scrollSpeed); return false;
+	});
+})(jQuery);
